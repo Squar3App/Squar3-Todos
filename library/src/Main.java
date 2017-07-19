8ac935a5643c30945656bf0d5e7196f2d13ac811
Soham <sohampardeshi@gmail.com>
2017-07-18 18:00:07 -0700

    Improved Commenting

--- library/src/Main.java
+++ library/src/Main.java
@@ -11,9 +11,9 @@ public class Main {
 		Mat img = Imgcodecs.imread("img/elephant-crop.jpg");
 		Mat templ = Imgcodecs.imread("img/pattern.jpg");
 
-		// Squar3 result = Squar3Processor.findSquar3(img, templ); 	// Fastest, least accurate
-		// Squar3 result = Squar3Processor.findHomology(img, templ, Squar3Processor.ORB); 	// Medium, medium accuracy
-		Squar3 result = Squar3Processor.findHomology(img, templ); 	// Slowest, good accuracy
+		// Squar3 result = Squar3Processor.findSquar3(img, templ); 								// Fastest, least accurate
+		// Squar3 result = Squar3Processor.findHomology(img, templ, Squar3Processor.ORB); 		// Medium, medium accuracy
+		Squar3 result = Squar3Processor.findHomology(img, templ); 								// Slowest, good accuracy
 		Mat cropped = result.crop();
 
 		Utils.show(img, "Before");
